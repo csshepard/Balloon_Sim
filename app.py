e84c14ff2535e79f5c0ab14227ef18697b455070
Chris Shepard <csshepard@gmail.com>
2015-04-03 17:33:48 -0400

    Added routes

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 from astral import Location
 from collections import namedtuple
-from flask import Flask
+from flask import Flask, render_template, redirect
 from flask_sqlalchemy import SQLAlchemy
 import datetime
 import requests
@@ -12,13 +12,14 @@ import re
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 db = SQLAlchemy(app)
+app.debug = True
 
 
 class Simulation(db.Model):
     uuid = db.Column(db.String(50), primary_key=True)
     site_id = db.Column(db.Integer, db.ForeignKey('launch_site.id'))
     launch_date = db.Column(db.DateTime)
-    create_date = db.Column(db.DateTime)
+    create_date = db.Column(db.Date)
     kml_file = db.Column(db.Text)
     landing_site = db.relationship('LandingSite', uselist=False, backref='simulation')
 
@@ -84,11 +85,9 @@ def get_uuid(place, launch_date, ascent_rate, burst_altitude, descent_speed):
                'ascent': ascent_rate,
                'burst': burst_altitude,
                'drag': descent_speed,
-               'submit': 'Run Prediction'}
+               'submit': 'Run+Prediction'}
     r = requests.post(url='http://predict.habhub.org/ajax.php?action=submitForm', data=payload)
-    data = r.json()
-    if data['valid']:
-        return data['uuid']
+    return r.json()
 
 
 def get_kml(uuid):
@@ -99,20 +98,17 @@ def get_kml(uuid):
 def get_landing_site(kml):
     ns = {'kml': "http://www.opengis.net/kml/2.2"}
     root = ET.fromstring(kml)
-    try:
-        document = root.find('kml:Document', ns)
-        for place in document.findall('kml:Placemark', ns):
-            if place.find('kml:Name', ns).text == 'Predicted Balloon Landing':
-                time_date = re.compile('(\d{2}:\d{2}).((\d{2}/){2}\d{4})')
-                land_text = place.find('kml:description', ns).text
-                land_time = time_date.search(land_text)
-                land_time = land_time.string[land_time.start():land_time.end()]
-                dtformat = '%H:%M %d/%m/%Y'
-                landing_time = datetime.datetime.strptime(land_time, dtformat)
-                land_coord = place.find('kml:Point', ns).find('kml:coordinates', ns).text
-                return land_coord, landing_time
-    except AttributeError:
-        pass
+    document = root.find('kml:Document', ns)
+    for place in document.findall('kml:Placemark', ns):
+        if place.find('kml:name', ns).text == 'Predicted Balloon Landing':
+            #time_date = re.compile('(\d{2}:\d{2}).((\d{2}/){2}\d{4})')
+            #land_text = place.find('kml:description', ns).text
+            #land_time = time_date.search(land_text)
+            #land_time = land_time.string[land_time.start():land_time.end()]
+            #dtformat = '%H:%M %d/%m/%Y'
+            #landing_time = datetime.datetime.strptime(land_time, dtformat)
+            land_coord = place.find('kml:Point', ns).find('kml:coordinates', ns).text
+            return land_coord #, landing_time
 
 
 @app.route('/')
@@ -123,10 +119,37 @@ def home():
 @app.route('/run_sim/<date>')
 def run_sim(date):
     for site in LOCATIONS:
+        site_row = LaunchSite.query.filter_by(name=site.name).one()
         launch_datetime = get_sunrise(site, datetime.datetime.strptime(date, '%d-%m-%Y'))
-        uuid = get_uuid(site, launch_datetime, ASCENT_RATE, BURST_ALTITUDE, DRAG)
-        kml = get_kml(uuid)
-        landing_site, landing_time = get_landing_site(kml)
+        uuid_data = get_uuid(site, launch_datetime, ASCENT_RATE, BURST_ALTITUDE, DRAG)
+        if uuid_data['valid'] == 'true':
+            kml = get_kml(uuid_data['uuid'])
+            landing_site = get_landing_site(kml)
+            landing_coords = landing_site.split(',')
+            sim = Simulation(uuid_data['uuid'], site_row.id, launch_datetime,
+                             datetime.date.today(), kml)
+            if Simulation.query.filter_by(uuid=uuid_data['uuid']) is None:
+                db.session.add(sim)
+                l_site = LandingSite(uuid_data['uuid'], landing_coords[1], landing_coords[0])
+                db.session.add(l_site)
+                db.session.commit()
+            return redirect('/view_sim/%s' % date)
+        else:
+            return uuid_data['error']
+
+
+@app.route('/view_sim/<date>')
+def view_sim(date):
+    date_split = date.split('-')
+    launch_date = datetime.date(int(date_split[2]), int(date_split[1]), int(date_split[0]))
+    sims = Simulation.query.filter_by(launch_date=launch_date)
+    return render_template('view_sim.html', sims=sims)
+
+
+@app.route('/kml/<uuid>')
+def return_kml(uuid):
+    file = Simulation.query.filter_by(uuid=uuid).one().kml_file
+    return file
 
 
 if __name__ == '__main__':

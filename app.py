72581ae52c9d62c4d5dfa7bd4639b3f2eb556594
Chris Shepard <csshepard@gmail.com>
2015-04-03 14:43:03 -0400

    Fixed app.config database key

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ import re
 
 
 app = Flask(__name__)
-app.config['SQALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 db = SQLAlchemy(app)
 
 

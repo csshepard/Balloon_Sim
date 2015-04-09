593f38d8009456858a20a497c5b6613e5a665560
Chris Shepard <CSSHEPARD@gmail.com>
2015-04-09 10:03:57 -0400

    turned of debug flag

--- app.py
+++ app.py
@@ -14,7 +14,6 @@ import xml.etree.ElementTree as et
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 db = SQLAlchemy(app)
-app.debug = True
 
 
 class Simulation(db.Model):
